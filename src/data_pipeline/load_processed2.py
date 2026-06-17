0a9f748e735d3132cd7ebe3ff5c05906af8a6505
Anya-Helle-Pritzl <s245208@dtu.dk>
2026-06-17 11:54:55 +0200

    rewrote load_processed to fit to new setup

--- src/data_pipeline/load_processed2.py
+++ src/data_pipeline/load_processed2.py
@@ -5,7 +5,7 @@ import json
 import pandas as pd
 import torch
 
-# %%
+# %% Loads
 
 def load_processed_dataset(dataset_name, representation, mode, processed_dir="src/data_pipeline/data/processed", fold=None): 
     processed_dir = Path(processed_dir) # converts the string path to a Path object for use later
@@ -20,6 +20,26 @@ def load_processed_dataset(dataset_name, representation, mode, processed_dir="sr
         train_path = processed_dir / f"{dataset_name}_{representation}_fold_{fold}_train.csv" # this and the next builds the file paths for a specific given fold (since we now checked whether one was specified)
         val_path = processed_dir / f"{dataset_name}_{representation}_fold_{fold}_test.csv"
 
-        train_df = pd.read_csv(train_path)
-        
+    train_df = pd.read_csv(train_path)
+    val_df = pd.read_csv(val_path)
 
+    metadata_path = processed_dir / f"{dataset_name}_{representation}_metadata.json"
+    with open(metadata_path, "r", encoding="utf-8") as f:
+        metadata = json.load(f)
+
+    return {
+        "train_df": train_df,
+        "val_df": val_df,
+        "metadata": metadata,
+    }
+
+# %% Conversion (converts neural models columns to numpy array of floats and then to PyTorch tensors)
+
+def dataframe_to_tensors(df, target_column="target", device="cpu"):
+    X = df.drop(columns=[target_column]).to_numpy(dtype="float32") # drops target, converts to numpy array of float
+    y = df[target_column].to_numpy(dtype="int64") # converts target column to numpy array of floats
+
+    X_tensor = torch.tensor(X, dtype=torch.float32, device=device) # this and the next converts both numpy arrays to PyTorch tensors and places them on the specified device
+    y_tensor = torch.tensor(y, dtype=torch.long, device=device)
+
+    return X_tensor, y_tensor
